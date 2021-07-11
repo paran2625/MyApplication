de0460540505d51aa5e1900fb45744367da0d0d4
paran2625 <paran2625@gmail.com>
2021-07-11 10:42:43 +0900

    test

---
+++ app/src/main/java/test/myPackage/MainActivity.java
@@ -0,0 +1,17 @@
+package test.myPackage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Test t = new Test();
+        //t.addDB();
+        //t.readDB();
+    }
+}
---
+++ app/src/main/java/test/myPackage/Test.java
@@ -0,0 +1,69 @@
+package test.myPackage;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class Test {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // FireStore Databaseに追加
+    public void addDB() {
+        Map<String, Object> user = new HashMap<>();
+
+        // 各フィールドのキー・値を指定
+        user.put("NAME", "PARAN2");
+        user.put("AGE", 26);
+        user.put("HEIGHT", 170);
+
+        // コレクションを指定(存在しない場合は新規作成)
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    // 読み込んだDB情報の返り値を調査しリターンしたい！
+    public void readDB() {
+        db.collection("users")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                        }
+                    } else {
+                        Log.w(TAG, "Error getting documents.", task.getException());
+                    }
+                }
+            });
+    }
+
+}
